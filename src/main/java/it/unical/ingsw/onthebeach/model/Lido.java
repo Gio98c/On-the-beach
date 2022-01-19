2b24b06dbaeeb66e8f5fb45bcbe212483a98954e
Giovanni <gllgnn01c28d086u@studenti.unical.com>
2022-01-19 17:26:42 +0100

    implementation class Lido, Prenotazione

--- src/main/java/it/unical/ingsw/onthebeach/model/Lido.java
+++ src/main/java/it/unical/ingsw/onthebeach/model/Lido.java
@@ -2,4 +2,78 @@ package it.unical.ingsw.onthebeach.model;
 
 public class Lido {
 
+	private String nome;
+	private String posizione;
+	private String numero;
+	private String email;
+	private String descrizione;
+	private byte[] foto;
+	private int numeroOmbrelloni;
+	private String usernameGestore;
+	
+	public Lido() {}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getPosizione() {
+		return posizione;
+	}
+	
+	public void setPosizione(String posizione) {
+		this.posizione = posizione;
+	}
+	
+	public String getNumero() {
+		return numero;
+	}
+	
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getDescrizione() {
+		return descrizione;
+	}
+	
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	
+	public byte[] getFoto() {
+		return foto;
+	}
+	
+	public void setFoto(byte[] foto) {
+		this.foto = foto;
+	}
+	
+	public int getNumeroOmbrelloni() {
+		return numeroOmbrelloni;
+	}
+	
+	public void setNumeroOmbrelloni(int numeroOmbrelloni) {
+		this.numeroOmbrelloni = numeroOmbrelloni;
+	}
+	
+	public String getUsernameGestore() {
+		return usernameGestore;
+	}
+	
+	public void setUsernameGestore(String usernameGestore) {
+		this.usernameGestore = usernameGestore;
+	}
 }
