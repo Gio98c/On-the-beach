e87161d0cf3f14ec1e4f21df197f126ed8351c0a
Gio98c <giovanirusso1998.gr@gmail.com>
2022-01-20 19:21:02 +0100

    update webContent

---
+++ src/main/java/it/unical/ingsw/onthebeach/controller/HomePage.java
@@ -0,0 +1,15 @@
+package it.unical.ingsw.onthebeach.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class HomePage {
+    @GetMapping("/")
+    public String homePage(HttpServletRequest req, HttpServletResponse resp) {
+        return "index";
+    }
+}
