34c98320b1cc5dd97e1d7f26f30afa70723101dc
Giovanni <gllgnn01c28d086u@studenti.unical.com>
2022-01-21 18:30:44 +0100

    implementation lidoDaoJDBC

--- src/main/java/it/unical/ingsw/onthebeach/persistenza/dao/jdbc/LidoDaoJDBC.java
+++ src/main/java/it/unical/ingsw/onthebeach/persistenza/dao/jdbc/LidoDaoJDBC.java
@@ -1,6 +1,10 @@
 package it.unical.ingsw.onthebeach.persistenza.dao.jdbc;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import it.unical.ingsw.onthebeach.model.Lido;
@@ -16,7 +20,32 @@ public class LidoDaoJDBC implements LidoDao{
 	
 	@Override
 	public List<Lido> findAll() {
+		List<Lido> lidi = new ArrayList<Lido>();
 		String query = "SELECT * FROM Lido;";
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(query);
+			rs = st.executeQuery();
+			while(rs.next()) {
+				Lido lido = new Lido();
+				lido.setNome(rs.getString("nome"));
+				lido.setPosizione(rs.getString("posizione"));
+				lido.setNumero(rs.getString("numero"));
+				lido.setEmail(rs.getString("email"));
+				lido.setDescrizione(rs.getString("descrizione"));
+				lido.setFoto(rs.getBytes("foto"));
+				lido.setNumeroOmbrelloni(rs.getInt("numero_ombrelloni"));
+				lido.setUsernameGestore(rs.getString("username_gestore"));
+				lidi.add(lido);
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			closeResult(rs);
+			closeStatement(st);
+		}
+		return lidi;
 	}
 
 	@Override
@@ -36,5 +65,21 @@ public class LidoDaoJDBC implements LidoDao{
 		// TODO Auto-generated method stub
 		return false;
 	}
-
+	
+	
+	private void closeStatement(PreparedStatement statement) {
+		try {
+			statement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void closeResult(ResultSet result) {
+		try {
+			result.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
