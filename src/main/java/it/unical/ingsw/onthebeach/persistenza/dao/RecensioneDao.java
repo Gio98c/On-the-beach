93716b8d6b61ced8c2122943032e911f822d557f
Luca Spadafora <luca.spadaforas@gmail.com>
2022-01-23 11:53:15 +0100

    completati principali UtenteDaoJDBC.java

--- src/main/java/it/unical/ingsw/onthebeach/persistenza/dao/RecensioneDao.java
+++ src/main/java/it/unical/ingsw/onthebeach/persistenza/dao/RecensioneDao.java
@@ -1,5 +1,13 @@
 package it.unical.ingsw.onthebeach.persistenza.dao;
+import it.unical.ingsw.onthebeach.model.Recensione;
+
+import java.util.List;
 
 public interface RecensioneDao {
-
+    public List<Recensione> findAll();
+    public Recensione findByPrimaryKey(long id);
+    public List<Recensione> findByUtente(String username);
+    public List<Recensione> findByLido(String nome);
+    public boolean saveOrUpdate(long id);
+    public boolean delete(long id);
 }
--- src/main/java/it/unical/ingsw/onthebeach/persistenza/dao/jdbc/RecensioneDaoJDBC.java
+++ src/main/java/it/unical/ingsw/onthebeach/persistenza/dao/jdbc/RecensioneDaoJDBC.java
@@ -1,12 +1,45 @@
 package it.unical.ingsw.onthebeach.persistenza.dao.jdbc;
 
+import it.unical.ingsw.onthebeach.model.Recensione;
 import it.unical.ingsw.onthebeach.persistenza.dao.RecensioneDao;
 
 import java.sql.Connection;
+import java.util.List;
 
 public class RecensioneDaoJDBC implements RecensioneDao {
     Connection conn;
     public RecensioneDaoJDBC(Connection conn){
         this.conn=conn;
+    }
+
+
+    @Override
+    public List<Recensione> findAll() {
+        return null;
+    }
+
+    @Override
+    public Recensione findByPrimaryKey(long id) {
+        return null;
+    }
+
+    @Override
+    public List<Recensione> findByUtente(String username) {
+        return null;
+    }
+
+    @Override
+    public List<Recensione> findByLido(String nome) {
+        return null;
+    }
+
+    @Override
+    public boolean saveOrUpdate(long id) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(long id) {
+        return false;
     }
 }
