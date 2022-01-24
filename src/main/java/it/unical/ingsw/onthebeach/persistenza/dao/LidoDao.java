4b09ca5f29626c9029c2f4edceca5bac4cd2d9e5
Giovanni <gllgnn01c28d086u@studenti.unical.com>
2022-01-24 16:25:30 +0100

    Implementation class LidoDao, LidoDaoJDBC, PrenotazioneDao, PrenotazioneDaoJDBC. Create and implement class IdBroker

--- src/main/java/it/unical/ingsw/onthebeach/persistenza/dao/LidoDao.java
+++ src/main/java/it/unical/ingsw/onthebeach/persistenza/dao/LidoDao.java
@@ -1,5 +1,13 @@
 package it.unical.ingsw.onthebeach.persistenza.dao;
 
+import java.util.List;
+
+import it.unical.ingsw.onthebeach.model.Lido;
+
 public interface LidoDao {
 
+	public List<Lido> findAll();
+	public Lido findByPrimaryKey(String nome);
+	public boolean saveOrUpdate(Lido lido);
+	public boolean delete(Lido lido);
 }
--- src/main/java/it/unical/ingsw/onthebeach/persistenza/dao/jdbc/PrenotazioneDaoJDBC.java
+++ src/main/java/it/unical/ingsw/onthebeach/persistenza/dao/jdbc/PrenotazioneDaoJDBC.java
@@ -1,5 +1,170 @@
 package it.unical.ingsw.onthebeach.persistenza.dao.jdbc;
 
-public class PrenotazioneDaoJDBC {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import it.unical.ingsw.onthebeach.model.Prenotazione;
+import it.unical.ingsw.onthebeach.persistenza.dao.PrenotazioneDao;
+
+public class PrenotazioneDaoJDBC implements PrenotazioneDao {
+
+	Connection conn;
+	
+	public PrenotazioneDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+	
+	@Override
+	public List<Prenotazione> findAll() {
+		List<Prenotazione> prenotazioni = new ArrayList<Prenotazione>();
+		String query = "SELECT * FROM prenotazione;";
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(query);
+			rs = st.executeQuery();
+			while(rs.next()) {
+				Prenotazione prenotazione = new Prenotazione();
+				prenotazione.setIdPrenotazione(rs.getLong("id_prenotazione"));
+				prenotazione.setPrezzoTotale(rs.getFloat("prezzo_totale"));
+				prenotazione.setDescrizione(rs.getString("descrizione"));
+				prenotazione.setDataPrenotazione(rs.getDate("data_prenotazione"));
+				prenotazione.setDataInizio(rs.getDate("data_inizio"));
+				prenotazione.setDataFine(rs.getDate("data_fine"));
+				prenotazione.setUsernameCliente(rs.getString("username_cliente"));
+				prenotazione.setNomeLido(rs.getString("nome_lido"));
+				prenotazioni.add(prenotazione);
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			closeResult(rs);
+			closeStatement(st);
+		}
+		return prenotazioni;
+	}
+
+	@Override
+	public Prenotazione findByPrimaryKey(long id) {
+		Prenotazione prenotazione = null;
+		String query = "SELECT * FROM prenotazione WHERE id_prenotazione=?;";
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(query);
+			st.setLong(1, id);
+			rs = st.executeQuery();
+			if(rs.next()) {
+				prenotazione = new Prenotazione();
+				prenotazione.setPrezzoTotale(rs.getFloat("prezzo_totale"));
+				prenotazione.setDescrizione(rs.getString("descrizione"));
+				prenotazione.setDataPrenotazione(rs.getDate("data_prenotazione"));
+				prenotazione.setDataInizio(rs.getDate("data_inizio"));
+				prenotazione.setDataFine(rs.getDate("data_fine"));
+				prenotazione.setUsernameCliente(rs.getString("username_cliente"));
+				prenotazione.setNomeLido(rs.getString("nome_lido"));
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			closeResult(rs);
+			closeStatement(st);
+		}
+		return prenotazione;
+	}
+
+	@Override
+	public List<Prenotazione> findByUsername(String username) {
+		List<Prenotazione> prenotazioni = new ArrayList<Prenotazione>();
+		String query = "SELECT * FROM prenotazione WHERE username_cliente=?;";
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(query);
+			st.setString(1, username);
+			rs = st.executeQuery();
+			while(rs.next()) {
+				Prenotazione prenotazione = new Prenotazione();
+				prenotazione.setIdPrenotazione(rs.getLong("id_prenotazione"));
+				prenotazione.setPrezzoTotale(rs.getFloat("prezzo_totale"));
+				prenotazione.setDescrizione(rs.getString("descrizione"));
+				prenotazione.setDataPrenotazione(rs.getDate("data_prenotazione"));
+				prenotazione.setDataInizio(rs.getDate("data_inizio"));
+				prenotazione.setDataFine(rs.getDate("data_fine"));
+				prenotazione.setNomeLido(rs.getString("nome_lido"));
+				prenotazioni.add(prenotazione);
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			closeResult(rs);
+			closeStatement(st);
+		}
+		return prenotazioni;
+	}
+
+	@Override
+	public boolean save(Prenotazione prenotazione) {
+		prenotazione.setIdPrenotazione(IdBroker.getId(conn));
+		String query = "INSERT INTO prenotazione(id_prenotazione, prezzo_totale, descrizione, data_prenotazione, data_inizio, "
+				+ "data_fine, username_cliente, nome_lido) VALUES(?,?,?,?,?,?,?,?);";
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(query);
+			st.setLong(1, prenotazione.getIdPrenotazione());
+			st.setFloat(2, prenotazione.getPrezzoTotale());
+			st.setString(3, prenotazione.getDescrizione());
+			st.setDate(4, prenotazione.getDataPrenotazione());
+			st.setDate(5, prenotazione.getDataInizio());
+			st.setDate(6,prenotazione.getDataFine());
+			st.setString(7, prenotazione.getUsernameCliente());
+			st.setString(8, prenotazione.getNomeLido());
+			st.executeUpdate();
+		} catch(SQLException e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			closeStatement(st);
+		}
+		return true;
+	}
+
+	@Override
+	public boolean delete(Prenotazione prenotazione) {
+		long id = prenotazione.getIdPrenotazione();
+		String query = "DELETE FROM prenotazione WHERE id_prenotazione=?;";
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(query);
+			st.setLong(1, id);
+			st.executeUpdate();
+		} catch(SQLException e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			closeStatement(st);
+		}
+		return true;
+	}
+
+	
+	private void closeStatement(PreparedStatement statement) {
+		try {
+			statement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void closeResult(ResultSet result) {
+		try {
+			result.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
