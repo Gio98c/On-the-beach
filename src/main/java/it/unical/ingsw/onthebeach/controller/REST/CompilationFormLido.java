1210e74e3557e8d01773dcf07f80cb1e6be75e32
Giovanni <gllgnn01c28d086u@studenti.unical.com>
2022-01-28 19:42:56 +0100

    CompilationFormLido

---
+++ src/main/java/it/unical/ingsw/onthebeach/controller/REST/CompilationFormLido.java
@@ -0,0 +1,41 @@
+package it.unical.ingsw.onthebeach.controller.REST;
+
+import it.unical.ingsw.onthebeach.Database;
+import it.unical.ingsw.onthebeach.model.Lido;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@RestController
+public class CompilationFormLido {
+
+    @PostMapping("/registrationLido")
+    public String addLido(HttpServletRequest req, HttpServletResponse resp, String nome, String usernameGestore, String posizione, String numero, String email, int numeroOmbrelloni, String foto, String descrizione) throws IOException {
+        Lido lido = new Lido();
+        lido.setNome(nome);
+        lido.setUsernameGestore(usernameGestore);
+        lido.setPosizione(posizione);
+        lido.setNumero(numero);
+        lido.setEmail(email);
+        lido.setNumeroOmbrelloni(numeroOmbrelloni);
+        lido.setFoto(foto);
+        lido.setDescrizione(descrizione);
+
+        try {
+            if(Database.getInstance().getLidoDao().saveOrUpdate(lido))
+                resp.sendRedirect("login");
+            else
+                return "registration";
+        } catch (SQLException e) {
+            e.printStackTrace();
+            resp.sendRedirect("login");
+        }
+
+        return null;
+    }
+}
